5ce370dc58814a3791a6da465e1f6f35cbb90b35
Paula Domingues <pdomingues@itba.edu.ar>
2021-09-02 12:11:18 -0300

    UserDao and UserService implemented.

--- models/src/main/java/ar/edu/itba/paw/models/user/User.java
+++ models/src/main/java/ar/edu/itba/paw/models/user/User.java
@@ -1,45 +1,18 @@
 package ar.edu.itba.paw.models.user;
 
 public class User {
+    private int userId;
     private String mail;
     private String username;
     private String password;
-    private String name;
-    private String profilePhotoURL;
+//    private String name;
+//    private String profilePhotoURL;
 
-    public User(String mail, String username, String password, String name, String profilePhotoURL) {
+
+    public User(int userId, String mail, String username, String password) {
+        this.userId = userId;
         this.mail = mail;
         this.username = username;
         this.password = password;
-        this.name = name;
-        this.profilePhotoURL = profilePhotoURL;
-    }
-
-    public User(String mail, String username, String password, String name) {
-        this.mail = mail;
-        this.username = username;
-        this.password = password;
-        this.name = name;
-        this.profilePhotoURL = "https://i.pinimg.com/736x/aa/f7/05/aaf705e06726ce3881288ae4be3ac5fe.jpg";
-    }
-
-    public String getMail() {
-        return mail;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getProfilePhotoURL() {
-        return profilePhotoURL;
     }
 }
