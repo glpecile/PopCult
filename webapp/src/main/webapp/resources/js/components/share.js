6a9cea3b8f78cda02f2bfbd79e6bafdc8727f5a9
Paula Domingues <pdomingues@itba.edu.ar>
2021-09-09 11:59:17 -0300

    Hotfix: cookies wont appear anymore when attempting to share URL

--- webapp/src/main/webapp/resources/js/components/share.js
+++ webapp/src/main/webapp/resources/js/components/share.js
@@ -12,11 +12,19 @@ function copyToClipboard() {
 }
 
 function tweetCurrentPage() {
-    window.open("https://twitter.com/share?url=" + encodeURIComponent(window.location.href) + "&text=" + document.title, '_blank');
+    let url = removeCookies(encodeURIComponent(window.location.href))
+    window.open("https://twitter.com/share?url=" + url + "&text=" + document.title, '_blank');
     // window.location.href = "https://twitter.com/intent/tweet?url=" + encodeURIComponent(window.location.href) + "&text=" + document.title;
     // return false;
 }
 
 function whatsappCurrentPage() {
-    window.open("https://api.whatsapp.com/send/?phone&text=Hey!%20check%20this%20content%20out!%20" + document.title + "%20in%20" + encodeURIComponent(window.location.href) + "&app_absent=0")
+    let url = removeCookies(encodeURIComponent(window.location.href))
+    window.open("https://api.whatsapp.com/send/?phone&text=Hey!%20check%20this%20content%20out!%20" + document.title + "%20in%20" + url + "&app_absent=0")
+}
+
+function removeCookies(originalURL){
+    let trimmedURL = originalURL.substring(0, originalURL.indexOf("%3B"))
+    console.log(originalURL)
+    return trimmedURL?trimmedURL:originalURL;
 }
