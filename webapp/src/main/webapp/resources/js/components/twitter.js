9bffe3f92490d317408619213b475b4e1d2459ef
Paula Domingues <pdomingues@itba.edu.ar>
2021-09-09 03:34:44 -0300

    Hotfix: Twitter now opens a new tab to share.
    Does not work with blocked settings on popups. Previous commit with window.location.href did but opened in the same tab.

--- webapp/src/main/webapp/resources/js/components/twitter.js
+++ webapp/src/main/webapp/resources/js/components/twitter.js
@@ -1,4 +1,5 @@
 function tweetCurrentPage() {
-    window.location.href = "https://twitter.com/intent/tweet?url=" + encodeURIComponent(window.location.href) + "&text=" + document.title;
-    return false;
+    window.open("https://twitter.com/share?url=" + encodeURIComponent(window.location.href) + "&text=" + document.title, '_blank');
+    // window.location.href = "https://twitter.com/intent/tweet?url=" + encodeURIComponent(window.location.href) + "&text=" + document.title;
+    // return false;
 }
