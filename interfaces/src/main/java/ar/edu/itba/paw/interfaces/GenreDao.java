e10e17271835a9e81680363301f86276dfa83422
Roberto J Catalan <rcatalan@itba.edu.ar>
2021-09-16 20:30:30 -0300

    PageContainer added for GenreDAO related functions.

--- interfaces/src/main/java/ar/edu/itba/paw/interfaces/GenreDao.java
+++ interfaces/src/main/java/ar/edu/itba/paw/interfaces/GenreDao.java
@@ -1,12 +1,14 @@
 package ar.edu.itba.paw.interfaces;
 
+import ar.edu.itba.paw.models.PageContainer;
+
 import java.util.List;
 import java.util.Optional;
 
 public interface GenreDao {
     List<String> getGenreByMediaId(int mediaId);
 
-    List<Integer> getMediaByGenre(int genreId, int page, int pageSize);
+    PageContainer<Integer> getMediaByGenre(int genreId, int page, int pageSize);
 
     Optional<Integer> getMediaCountByGenre(int genreId);
 
