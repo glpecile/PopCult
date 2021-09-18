49f79a55b3321123f156b88246aa0ba71ec70220
Julian Arce <juarce@itba.edu.ar>
2021-09-18 15:54:02 -0300

    GenreController refactor.
    Bugfix: bad SELECT in genreDao.

--- webapp/src/main/java/ar/edu/itba/paw/webapp/controller/GenreController.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controller/GenreController.java
@@ -37,17 +37,19 @@ public class GenreController {
     @RequestMapping("/genre/{genre}")
     public ModelAndView genre(@PathVariable(value = "genre") final String genre,
                               @RequestParam(value = "page", defaultValue = "1") final int page) {
-        final ModelAndView mav = new ModelAndView("genreView");
+        final ModelAndView mav = new ModelAndView("genre");
         final String normalizedGenre = genre.replaceAll("\\s+", "").toUpperCase();
         final int genreOrdinal = Genre.valueOf(normalizedGenre).ordinal() + 1;
-        final PageContainer<Integer> mediaIdList = genreService.getMediaByGenreIds(genreOrdinal, page - 1, itemsPerPage);
-        final List<Media> mediaList = mediaService.getById(mediaIdList.getElements());
+//        final PageContainer<Integer> mediaIdList = genreService.getMediaByGenreIds(genreOrdinal, page - 1, itemsPerPage);
+        final PageContainer<Media> mediaPageContainer = genreService.getMediaByGenre(genreOrdinal, page - 1, itemsPerPage);
+//        final List<Media> mediaList = mediaService.getById(mediaIdList.getElements());
         final List<MediaList> genreLists = listsService.getListsContainingGenre(genreOrdinal, listInPage, minimumMediaMatches);
         final List<ListCover> listCovers = getListCover(genreLists, listsService, mediaService);
         mav.addObject("genreName", Genre.valueOf(normalizedGenre).getGenre());
-        mav.addObject("mediaList", mediaList);
+        mav.addObject("mediaPageContainer", mediaPageContainer);
+//        mav.addObject("mediaList", mediaList);
         mav.addObject("genreLists", listCovers);
-        mav.addObject("mediaIdListContainer", mediaIdList);
+//        mav.addObject("mediaIdListContainer", mediaIdList);
         return mav;
     }
 }
