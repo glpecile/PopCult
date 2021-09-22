e1c87785f38d6babfe5045eccdce5bc1246cfadb
Paula Domingues <pdomingues@itba.edu.ar>
2021-09-22 17:53:16 -0300

    Bugfix: User watchlist displays correctly

--- persistence/src/main/java/ar/edu/itba/paw/persistence/WatchDaoJdbcImpl.java
+++ persistence/src/main/java/ar/edu/itba/paw/persistence/WatchDaoJdbcImpl.java
@@ -91,11 +91,11 @@ public class WatchDaoJdbcImpl implements WatchDao {
         return new PageContainer<>(elements, page, pageSize, totalCount);
     }
 
-    @Override
-    public Optional<Integer> getWatchedMediaCount(int userId) {
-        return jdbcTemplate.query("SELECT COUNT(*) AS count FROM towatchmedia WHERE userId = ? AND watchDate IS NOT NULL", new Object[]{userId}, COUNT_ROW_MAPPER).stream().findFirst();
-
-    }
+//    @Override
+//    public Optional<Integer> getWatchedMediaCount(int userId) {
+//        return jdbcTemplate.query("SELECT COUNT(*) AS count FROM towatchmedia WHERE userId = ? AND watchDate IS NOT NULL", new Object[]{userId}, COUNT_ROW_MAPPER).stream().findFirst();
+//
+//    }
 
 //    @Override
 //    public PageContainer<Integer> getToWatchMediaIdIds(int userId, int page, int pageSize) {
@@ -106,13 +106,13 @@ public class WatchDaoJdbcImpl implements WatchDao {
 
     @Override
     public PageContainer<Media> getToWatchMediaId(int userId, int page, int pageSize) {
-        List<Media> elements = jdbcTemplate.query("SELECT * FROM towatchmedia NATURAL JOIN media WHERE watchDate IS NULL OFFSET ? LIMIT ?", new Object[]{page * pageSize, pageSize}, MEDIA_ROW_MAPPER);
+        List<Media> elements = jdbcTemplate.query("SELECT * FROM towatchmedia NATURAL JOIN media WHERE userId = ? AND watchDate IS NULL ORDER BY watchDate DESC OFFSET ? LIMIT ?", new Object[]{userId , page * pageSize, pageSize}, MEDIA_ROW_MAPPER);
         int totalCount = jdbcTemplate.query("SELECT COUNT(*) AS count FROM towatchmedia WHERE userId = ? AND watchDate IS NULL", new Object[]{userId}, COUNT_ROW_MAPPER).stream().findFirst().orElse(0);
         return new PageContainer<>(elements,page,pageSize,totalCount);
     }
 
-    @Override
-    public Optional<Integer> getToWatchMediaCount(int userId) {
-        return jdbcTemplate.query("SELECT COUNT(*) AS count FROM towatchmedia WHERE userId = ? AND watchDate IS NULL", new Object[]{userId}, COUNT_ROW_MAPPER).stream().findFirst();
-    }
+//    @Override
+//    public Optional<Integer> getToWatchMediaCount(int userId) {
+//        return jdbcTemplate.query("SELECT COUNT(*) AS count FROM towatchmedia WHERE userId = ? AND watchDate IS NULL", new Object[]{userId}, COUNT_ROW_MAPPER).stream().findFirst();
+//    }
 }
