b6a63b8f3442e06fb34d4a383b7c2845ce794d1e
Paula Domingues <pdomingues@itba.edu.ar>
2021-09-27 16:52:58 -0300

    Mailing implemented when receiving a new collab request

--- services/src/main/java/ar/edu/itba/paw/services/CollaborativeListsServiceImpl.java
+++ services/src/main/java/ar/edu/itba/paw/services/CollaborativeListsServiceImpl.java
@@ -1,21 +1,51 @@
 package ar.edu.itba.paw.services;
 
-import ar.edu.itba.paw.interfaces.CollaborativeListService;
-import ar.edu.itba.paw.interfaces.CollaborativeListsDao;
+import ar.edu.itba.paw.interfaces.*;
 import ar.edu.itba.paw.models.PageContainer;
 import ar.edu.itba.paw.models.collaborative.Request;
+import ar.edu.itba.paw.models.lists.MediaList;
+import ar.edu.itba.paw.models.user.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
+import java.util.Map;
 
 @Service
 public class CollaborativeListsServiceImpl implements CollaborativeListService {
     @Autowired
     private CollaborativeListsDao collaborativeListsDao;
+    @Autowired
+    private EmailService emailService;
+    @Autowired
+    private UserDao userDao;
+    @Autowired
+    private ListsDao listsDao;
+
+    private final MessageSource messageSource;
+
+    @Autowired
+    public CollaborativeListsServiceImpl(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
 
     @Override
     public Request makeNewRequest(int listId, int userId) {
+        try {
+            User user = userDao.getById(userId).orElseThrow(RuntimeException::new);
+            MediaList list = listsDao.getMediaListById(listId).orElseThrow(RuntimeException::new);
+            User to = userDao.getById(list.getUserId()).orElseThrow(RuntimeException::new);
+            final Map<String, Object> mailMap = new HashMap<>();
+            mailMap.put("listname", list.getListName());
+            mailMap.put("username", user.getUsername());
+            final String subject = messageSource.getMessage("collabEmail.subject", null, Locale.getDefault());
+            emailService.sendEmail(to.getEmail(), subject, "collaborationRequest.html", mailMap);
+        } catch (RuntimeException e) {
+            //TODO log correspondiente
+        }
         return collaborativeListsDao.makeNewRequest(listId, userId);
     }
 
