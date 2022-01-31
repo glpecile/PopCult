5b79711ba98570336d30f24b25410b7b6b597efd
Julian Arce <juarce@itba.edu.ar>
2022-01-31 20:51:22 -0300

    fix: @NotNull added to input Dto fields.

--- webapp/src/main/java/ar/edu/itba/paw/webapp/dto/input/UserEditDto.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/dto/input/UserEditDto.java
@@ -1,10 +1,12 @@
 package ar.edu.itba.paw.webapp.dto.input;
 
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 public class UserEditDto {
 
+    @NotNull
     @Size(min = 3, max = 100)
     @Pattern(regexp = "[a-zA-Z0-9\\s]+")
     private String name;
