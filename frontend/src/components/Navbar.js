9f5ebcbddf24748d79856248f3786e48bbb250cb
Paula Domingues <pdomingues@itba.edu.ar>
2022-02-03 16:50:02 -0300

    fix: navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -3,12 +3,12 @@ import {NavLink} from "react-router-dom";
 function Navbar() {
     return (
         <nav
-            className="pb-3 pt-3 navbar navbar-expand-lg w-full navbar-dark bg-dark text-white shadow-md bg-gradient-to-r from-yellow-500 to-purple-900">
-            <div className="flex justify-between items-center sm:px-12 px-16">
+            className="mb-3 relative navbar navbar-expand-lg w-full navbar-dark bg-dark text-white shadow-md bg-gradient-to-r from-yellow-500 to-purple-900">
+            <div className="container-fluid flex sm:px-12 px-16">
                 <NavLink className="navbar-brand m-0 p-0" to='/'>
                     <img
                         className="transition duration-500 ease-in-out transform hover:-translate-y-1 hover:rotate-2 w-52"
-                        src={require('./PopCultCompleteLogo.png')}
+                        src={require('../images/PopCultCompleteLogo.png')}
                         alt="popcult_text_logo"/>
                 </NavLink>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
@@ -20,9 +20,8 @@ function Navbar() {
                 <div
                     className="collapse navbar-collapse flex space-x-8 justify-center items-center text-center sm:justify-end"
                     id="navbarScroll">
-                    <ul className="navbar-nav flex ms-auto my-2 my-lg-0">
-                        {/*si se saca el flex de esta ul los elementos se ponen verticales*/}
-                        <li className=" flex nav-item transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-105">
+                    <ul className="navbar-nav ms-auto my-2 my-lg-0">
+                        <li className="nav-item transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-105">
                             <NavLink className="nav-link active text-lg lg:text-right" aria-current="page"
                                      to='/media/films'>Films</NavLink>
                         </li>
