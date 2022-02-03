a4b9ef4d668b28067654f08e5f00f798f635ff1c
Paula Domingues <pdomingues@itba.edu.ar>
2022-02-03 14:36:16 -0300

    feat: navbar styling added - needs retouch

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,25 +1,44 @@
-import {Link} from "react-router-dom";
+import {NavLink} from "react-router-dom";
 
 function Navbar() {
-    return (<header>
-        <div>React Routing</div>
-        <nav>
-            <ul>
-                <li>
-                    <Link to='/'>Home</Link>
-                </li>
-                <li>
-                    <Link to='/media/films'>Films</Link>
-                </li>
-                <li>
-                    <Link to='/media/series'>Series</Link>
-                </li>
-                <li>
-                    <Link to='/lists'>Lists</Link>
-                </li>
-            </ul>
+    return (
+        <nav
+            className="pb-3 pt-3 navbar navbar-expand-lg w-full navbar-dark bg-dark text-white shadow-md bg-gradient-to-r from-yellow-500 to-purple-900">
+            <div className="flex justify-between items-center sm:px-12 px-16">
+                <NavLink className="navbar-brand m-0 p-0" to='/'>
+                    <img
+                        className="transition duration-500 ease-in-out transform hover:-translate-y-1 hover:rotate-2 w-52"
+                        src={require('./PopCultCompleteLogo.png')}
+                        alt="popcult_text_logo"/>
+                </NavLink>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#navbarScroll"
+                        aria-controls="navbarScroll"
+                        aria-expanded="false" aria-label="Toggle navigation">
+                    <i className="fas fa-bars transition duration-500 ease-in-out transform focus:-translate-y-1 focus:scale-105"/>
+                </button>
+                <div
+                    className="collapse navbar-collapse flex space-x-8 justify-center items-center text-center sm:justify-end"
+                    id="navbarScroll">
+                    <ul className="navbar-nav flex ms-auto my-2 my-lg-0">
+                        {/*si se saca el flex de esta ul los elementos se ponen verticales*/}
+                        <li className=" flex nav-item transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-105">
+                            <NavLink className="nav-link active text-lg lg:text-right" aria-current="page"
+                                     to='/media/films'>Films</NavLink>
+                        </li>
+                        <li className="nav-item transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-105">
+                            <NavLink className="nav-link active text-lg lg:text-right" aria-current="page"
+                                     to='/media/series'>Series</NavLink>
+                        </li>
+                        <li className="nav-item transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-105">
+                            <NavLink className="nav-link active text-lg lg:text-right" aria-current="page"
+                                     to='/lists'>Lists</NavLink>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </nav>
-    </header>);
+    );
 }
 
 export default Navbar;
