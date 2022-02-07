1d0c7c428ac06536b4053ae686aacb5935b590c1
Paula Domingues <pdomingues@itba.edu.ar>
2022-02-07 11:40:39 -0300

    feat: fontawesome 5.15.4 installed

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,6 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.js';
+import '@fortawesome/fontawesome-free/js/all.js'
 
 ReactDOM.render(
     <React.StrictMode>
