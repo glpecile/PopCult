4c6aad99059ccde1eafa405fa6be823087d9ec0f
Gian <gpecile@itba.edu.ar>
2022-02-07 11:27:22 -0300

    style: checkbox and padding.

--- frontend/src/components/login/LoginForm.js
+++ frontend/src/components/login/LoginForm.js
@@ -25,20 +25,22 @@ function LoginForm(props) {
 
     return (
         <form className="space-y-4" onSubmit={submitHandler}>
-            <div className="flex flex-col justify-center items-center">
+            <div className="flex flex-col">
                 <label className="py-2 text-semibold w-full"/> Username:
-                <input className="form-control shadow-sm" type="text" value={enteredUsername}
+                <input className="form-control shadow-sm accent-purple-400" type="text" value={enteredUsername}
                        pattern="[a-zA-Z0-9]+" minLength={4} maxLength={100}
                        onChange={UsernameChangeHandler}/>
                 <label className="py-2 text-semibold w-full"/>Password:
                 <input className="form-control shadow-sm" type="password" value={enteredPassword}
                        minLength={8} maxLength={100}
                        onChange={PasswordChangeHandler}/>
-                <label className="py-2 text-semibold w-full"/>Remember me
-                <input className="shadow-sm" type="checkbox" defaultChecked={enteredRememberMe}
-                       onChange={RememberMeChangeHandler}/>
+                <div className="flex py-2 justify-start">
+                    <input className="shadow-sm mt-1.5 accent-purple-400" type="checkbox" defaultChecked={enteredRememberMe}
+                           onChange={RememberMeChangeHandler}/>
+                    <label className="text-semibold pl-1.5"/>Remember me
+                </div>
                 <button className="btn btn-secondary my-2 w-full" type="submit">
-                    Login
+                    Log In
                 </button>
             </div>
         </form>
--- frontend/src/components/login/LoginWrapper.js
+++ frontend/src/components/login/LoginWrapper.js
@@ -8,9 +8,10 @@ function LoginWrapper(props) {
                 <Link className="flex justify-center items-center pt-16" to='/'>
                     <img className="w-32" src={require("../../images/PopCultLogo.png")} alt="popcult_logo"/>
                 </Link>
-                <h2 className="font-bold text-4xl text-center text-white py-2">Login</h2>
+                <h2 className="font-bold text-4xl text-center text-white py-2">Log In</h2>
                 <div className="w-full max-w-xs mx-auto mt-8 px-4 font-sans rounded-lg shadow-lg bg-white p-2 my-8">
-                    {props.children}</div>
+                    {props.children}
+                </div>
             </div>
         </div>
     </section>);
