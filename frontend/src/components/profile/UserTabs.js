f0cc6a4ee10769d3ae84ac23f998717008955bf3
Paula Domingues <pdomingues@itba.edu.ar>
2022-02-10 21:24:27 -0300

    fix: code optimization

--- frontend/src/components/profile/UserTabs.js
+++ frontend/src/components/profile/UserTabs.js
@@ -13,87 +13,50 @@ const UserTabs = (props) => {
     const [userWatchedMediaActive, setUserWatchedMediaActive] = useState(false);
     const [userToWatchMediaActive, setUserToWatchMediaActive] = useState(false);
 
-    const UserListsTab = () => {
-        return (userListsActive ? (
-            <button
-                className="text-gray-600 py-2 px-6 block hover:text-purple-500 focus:outline-none text-purple-500 border-b-2 font-medium border-purple-500">
-                {props.username}'s Lists
-            </button>) : (<button onClick={() => {
-            setUserListsActive(true)
-            setUserFavMediaActive(false);
-            setUserFavListsActive(false);
-            setUserWatchedMediaActive(false);
-            setUserToWatchMediaActive(false);
-        }}
-                                  className="text-gray-600 py-2 px-6 block hover:text-purple-500 focus:outline-none">
-            {props.username}'s Lists
-        </button>));
+    const setTabsActiveFalse = () => {
+        setUserListsActive(false);
+        setUserFavMediaActive(false);
+        setUserFavListsActive(false);
+        setUserWatchedMediaActive(false);
+        setUserToWatchMediaActive(false);
     }
-
-    const UserFavMediaTab = () => {
-        return (userFavMediaActive ? (
+    const toReturnValue = (bool, title, setTabActiveTrue) => {
+        return (bool ? (
             <button
                 className="text-gray-600 py-2 px-6 block hover:text-purple-500 focus:outline-none text-purple-500 border-b-2 font-medium border-purple-500">
-                Favorite Media
+                {title}
             </button>) : (
             <button onClick={() => {
-                setUserListsActive(false);
-                setUserFavMediaActive(true);
-                setUserFavListsActive(false);
-                setUserWatchedMediaActive(false);
-                setUserToWatchMediaActive(false);
-            }}
-                    className="text-gray-600 py-2 px-6 block hover:text-purple-500 focus:outline-none">
-                Favorite Media
+                setTabsActiveFalse();
+                setTabActiveTrue(true);
+            }} className="text-gray-600 py-2 px-6 block hover:text-purple-500 focus:outline-none">
+                {title}
             </button>));
     }
 
+    const UserListsTab = () => {
+        const title = props.username + "'s Lists"
+        return toReturnValue(userListsActive, title, setUserListsActive);
+    }
+
+    const UserFavMediaTab = () => {
+        const title = "Favorite Media";
+        return toReturnValue(userFavMediaActive, title, setUserFavMediaActive);
+    }
+
     const UserFavListsTab = () => {
-        return (userFavListsActive ? (
-            <button
-                className="text-gray-600 py-2 px-6 block hover:text-purple-500 focus:outline-none text-purple-500 border-b-2 font-medium border-purple-500">
-                Favorite Lists
-            </button>
-        ) : (
-            <button onClick={() => {
-                setUserListsActive(false);
-                setUserFavMediaActive(false);
-                setUserFavListsActive(true);
-                setUserWatchedMediaActive(false);
-                setUserToWatchMediaActive(false);
-            }}
-                    className="text-gray-600 py-2 px-6 block hover:text-purple-500 focus:outline-none">
-                Favorite Lists
-            </button>
-        ));
+        const title = "Favorite Lists";
+        return toReturnValue(userFavListsActive, title, setUserFavListsActive);
     }
 
     const UserWatchedMediaTab = () => {
-        return (userWatchedMediaActive ? (<button
-            className="text-gray-600 py-2 px-6 block hover:text-purple-500 focus:outline-none text-purple-500 border-b-2 font-medium border-purple-500">
-            Watched Media </button>) : (
-            <button onClick={() => {
-                setUserListsActive(false);
-                setUserFavMediaActive(false);
-                setUserFavListsActive(false);
-                setUserWatchedMediaActive(true);
-                setUserToWatchMediaActive(false);
-            }} className="text-gray-600 py-2 px-6 block hover:text-purple-500 focus:outline-none">
-                Watched Media </button>));
+        const title = "Watched Media";
+        return toReturnValue(userWatchedMediaActive, title, setUserWatchedMediaActive);
     }
 
     const UserToWatchMediaTab = () => {
-        return (userToWatchMediaActive ? (<button
-            className="text-gray-600 py-2 px-6 block hover:text-purple-500 focus:outline-none text-purple-500 border-b-2 font-medium border-purple-500">
-            To Watch Media </button>) : (
-            <button onClick={() => {
-                setUserListsActive(false);
-                setUserFavMediaActive(false);
-                setUserFavListsActive(false);
-                setUserWatchedMediaActive(false);
-                setUserToWatchMediaActive(true);
-            }} className="text-gray-600 py-2 px-6 block hover:text-purple-500 focus:outline-none">
-                To Watch Media </button>));
+        const title = "To Watch Media";
+        return toReturnValue(userToWatchMediaActive, title, setUserToWatchMediaActive);
     }
     return (<>
             <div className="flex justify-center items-center bg-transparent py-4">
