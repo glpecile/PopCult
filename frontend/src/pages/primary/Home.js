823be08e87782c22d4ff23b7fd0622f59ef8fb1b
Paula Domingues <pdomingues@itba.edu.ar>
2022-02-15 15:47:37 -0300

    feat: first attempt to connect with api

--- frontend/src/pages/primary/Home.js
+++ frontend/src/pages/primary/Home.js
@@ -1,5 +1,8 @@
 import Loader from "../secondary/errors/Loader";
+import MediaServices from "../../services/MediaServices";
 
 export default function Home() {
+    const hello = MediaServices();
+    console.log(hello)
     return <Loader/>;
 }
