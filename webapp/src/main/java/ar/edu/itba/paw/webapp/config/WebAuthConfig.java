a183b3bec97b1069bb6c6621ed4d14b75cd7e050
Julian Arce <juarce@itba.edu.ar>
2022-02-15 18:18:35 -0300

    chore: CORS enabled in api.

--- webapp/src/main/java/ar/edu/itba/paw/webapp/config/WebAuthConfig.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/config/WebAuthConfig.java
@@ -29,10 +29,16 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.access.expression.DefaultWebSecurityExpressionHandler;
 import org.springframework.security.web.access.expression.WebExpressionVoter;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+
+import static org.springframework.web.cors.CorsConfiguration.ALL;
 
 /**
  * https://www.toptal.com/spring/spring-security-tutorial
@@ -40,7 +46,6 @@ import java.util.List;
  */
 @ComponentScan("ar.edu.itba.paw.webapp.auth")
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
 @Configuration
 public class WebAuthConfig extends WebSecurityConfigurerAdapter {
 
@@ -108,6 +113,17 @@ public class WebAuthConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public JwtTokenUtil jwtTokenUtil(@Value("classpath:jwt.key") Resource jwtKeyResource) throws IOException {
         return new JwtTokenUtil(jwtKeyResource);
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Collections.singletonList(ALL));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.addAllowedHeader(ALL);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
     // Expose authentication manager bean
@@ -197,7 +213,7 @@ public class WebAuthConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/**").permitAll()
 
                 // Disable CSRF
-                .and().csrf().disable()
+                .and().cors().and().csrf().disable()
 
                 // Add JWT Token Filter
                 .addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
