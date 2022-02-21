4a1b38bc9b149bdeced17433b673df1ed491b3c5
Paula Domingues <pdomingues@itba.edu.ar>
2022-02-21 16:09:43 -0300

    fix: keeping context on refresh

--- frontend/src/store/AuthContext.js
+++ frontend/src/store/AuthContext.js
@@ -1,5 +1,6 @@
 import {useState} from "react";
 import React from "react";
+import jwtDecode from "jwt-decode";
 
 const AuthContext = React.createContext({
     isLoggedIn: false,
@@ -10,28 +11,32 @@ const AuthContext = React.createContext({
 });
 
 export const AuthContextProvider = (props) => {
-    const [isLoggedIn, setLoggedIn] = useState(false);
-    const [authKey, setAuthKey] = useState('');
-    const [username, setUsername] = useState('');
+    const [isLoggedIn, setLoggedIn] = useState(localStorage.hasOwnProperty("userAuthToken"));
+    const token = isLoggedIn ? JSON.parse(localStorage.getItem("userAuthToken")) : '';
+    const [authKey, setAuthKey] = useState(token);
+    const [username, setUsername] = useState(jwtDecode(token).sub);
 
     const logoutHandler = () => {
         setLoggedIn(false);
         setAuthKey('');
         setUsername('');
     }
+
     const loginHandler = (authKey, username) => {
         setAuthKey(authKey);
         setUsername(username);
         setLoggedIn(true);
         console.log(username);
     }
-    return <AuthContext.Provider value={{
-        isLoggedIn: isLoggedIn,
-        onLogout: logoutHandler,
-        onLogin: loginHandler,
-        authKey: authKey,
-        username: username
-    }}>{props.children}</AuthContext.Provider>
+
+    return <AuthContext.Provider
+        value={{
+            isLoggedIn: isLoggedIn,
+            onLogout: logoutHandler,
+            onLogin: loginHandler,
+            authKey: authKey,
+            username: username
+        }}>{props.children}</AuthContext.Provider>
 }
 
 export default AuthContext;
