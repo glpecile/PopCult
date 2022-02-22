4e2fee4fe3c846f8f6af1c29c196e122531219a2
Gian <gpecile@itba.edu.ar>
2022-02-22 15:17:01 -0300

    feat: dropdown menu redesigned.

--- frontend/src/components/navigation/DropdownMenu.js
+++ frontend/src/components/navigation/DropdownMenu.js
@@ -1,55 +1,114 @@
 import {NavLink} from "react-router-dom";
 import {useTranslation} from "react-i18next";
 import {motion} from "framer-motion";
-import {useContext} from "react";
+import {useContext, useState} from "react";
 import AuthContext from "../../store/AuthContext";
+import ArrowDropDownOutlinedIcon from '@mui/icons-material/ArrowDropDownOutlined';
+import ArrowDropUpOutlinedIcon from '@mui/icons-material/ArrowDropUpOutlined';
+import {Menu} from "@mui/material";
+import MenuItem from '@mui/material/MenuItem';
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
+import ListOutlinedIcon from '@mui/icons-material/ListOutlined';
+import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNoneOutlined';
+import AdminPanelSettingsOutlinedIcon from '@mui/icons-material/AdminPanelSettingsOutlined';
+import LogoutOutlinedIcon from '@mui/icons-material/LogoutOutlined';
 
 const DropdownMenu = () => {
     const authContext = useContext(AuthContext);
+    const [anchorEl, setAnchorEl] = useState(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
 
     const [t] = useTranslation();
     return (
-        <motion.li whileHover={{scale: 1.1}}
-                   className="z-50 nav-item dropdown">
+        <>
             <motion.div whileTap={{scale: 0.9}}
-                        className="nav-link dropdown-toggle active text-lg lg:text-right"
-                        id="navbarDropdownMenuLink"
-                        role="button"
-                        data-bs-toggle="dropdown" aria-expanded="false">
-                {authContext.username}
+                        whileHover={{scale: 1.1}}
+                        className="text-lg lg:text-right p-2"
+                        onClick={handleClick}
+                        aria-controls={open ? 'account-menu' : undefined}
+                        aria-haspopup="true"
+                        aria-expanded={open ? 'true' : undefined}
+            >
+                {authContext.username}{anchorEl ? <ArrowDropUpOutlinedIcon/> : <ArrowDropDownOutlinedIcon/>}
             </motion.div>
-            <ul className="dropdown-menu dropdown-menu-light" aria-labelledby="navbarDropdownMenuLink">
-                <li>
-                    <NavLink to='/' className="dropdown-item">
-                        {t('nav_home')}
+
+            <Menu
+                className="px-2"
+                anchorEl={anchorEl}
+                id="account-menu"
+                open={open}
+                onClose={handleClose}
+                onClick={handleClose}
+                PaperProps={{
+                    elevation: 0,
+                    sx: {
+                        overflow: 'visible',
+                        filter: 'drop-shadow(0px 2px 4px rgba(0,0,0,0.32))',
+                        mt: 1.5,
+                        '& .MuiAvatar-root': {
+                            width: 32,
+                            height: 32,
+                            ml: -0.5,
+                            mr: 1,
+                        },
+                        '&:before': {
+                            content: '""',
+                            display: 'block',
+                            position: 'absolute',
+                            top: 0,
+                            right: 14,
+                            width: 10,
+                            height: 10,
+                            bgcolor: 'background.paper',
+                            transform: 'translateY(-50%) rotate(45deg)',
+                            zIndex: 0,
+                        },
+                    },
+                }}
+                transformOrigin={{horizontal: 'right', vertical: 'top'}}
+                anchorOrigin={{horizontal: 'right', vertical: 'bottom'}}
+            >
+                <MenuItem>
+                    <NavLink className="stretched-link text-slate-800" to='/'>
+                        <HomeOutlinedIcon/> {t('nav_home')}
                     </NavLink>
-                </li>
-                <li>
-                    <NavLink className="dropdown-item" to={'/user/' + authContext.username}>
-                        {t('nav_profile')}
+                </MenuItem>
+                <MenuItem>
+                    <NavLink className="stretched-link text-slate-800" to={'/user/' + authContext.username}>
+                        <AccountCircleOutlinedIcon/> {t('nav_profile')}
                     </NavLink>
-                </li>
-                <li>
-                    <NavLink className="dropdown-item" to={'/user/a'}>
-                        {t('nav_my_lists')}
+                </MenuItem>
+                <MenuItem>
+                    <NavLink className="stretched-link text-slate-800" to={'/user/a'}>
+                        <ListOutlinedIcon/> {t('nav_my_lists')}
                     </NavLink>
-                </li>
-                <li>
-                    <NavLink className="dropdown-item" to={'/user/b'}>
-                        {t('nav_notifications')}
+                </MenuItem>
+                <MenuItem>
+                    <NavLink className="stretched-link text-slate-800" to={'/user/b'}>
+                        <NotificationsNoneOutlinedIcon/> {t('nav_notifications')}
                     </NavLink>
-                </li>
-                <li>
-                    <NavLink className="dropdown-item" to='/admin'>
-                        {t('nav_admin')}
+                </MenuItem>
+                <MenuItem>
+                    <NavLink className="stretched-link text-slate-800" to='/admin'>
+                        <AdminPanelSettingsOutlinedIcon/> {t('nav_admin')}
                     </NavLink>
-                </li>
-                <li>
-                    <NavLink className="dropdown-item" to='/login' onClick={authContext.onLogout}>
-                        {t('nav_sign_out')}
+                </MenuItem>
+                <MenuItem>
+                    <NavLink className="stretched-link text-slate-800" to='/login' onClick={authContext.onLogout}>
+                        <LogoutOutlinedIcon/> {t('nav_sign_out')}
                     </NavLink>
-                </li>
-            </ul>
-        </motion.li>);
+                </MenuItem>
+            </Menu>
+        </>
+    )
+        ;
 }
 export default DropdownMenu;
