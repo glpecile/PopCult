63699b0fa8cd003e99fa2145559b39d2b65ef73e
Julian Arce <juarce@itba.edu.ar>
2022-02-25 19:35:52 -0300

    chore: User api and service.

--- frontend/src/api/UserApi.js
+++ frontend/src/api/UserApi.js
@@ -6,33 +6,262 @@ const userApi = (() => {
         return api.post('/authenticate', {username: username, password: password});
     }
 
+    const getUsers = ({page, pageSize, userRole, banned}) => {
+        return api.get(`/users`,
+            {
+                params: {
+                    'page': page,
+                    'page-size': pageSize,
+                    ...(userRole && {'role': userRole}),
+                    ...(banned && {'banned': banned})
+                }
+            });
+    }
+
     const getUser = (username) => {
         return api.get(`/users/${username}`);
     }
 
-    const uploadUserImage = ({username, formData}) => {
-        return api.put(`/users/${username}/image`, formData);
-    }
-
-    const editUser = ({username, name}) => {
-        return api.put(`users/${username}`, {name: name});
+    const createUser = ({email, username, password, name}) => {
+        return api.post(`/users`,
+            {
+                'email': email,
+                'username': username,
+                'password': password,
+                'name': name
+            });
     }
 
     const deleteUser = (username) => {
         return api.delete(`/users/${username}`);
     }
 
+    const editUser = ({username, name}) => {
+        return api.put(`users/${username}`, {'name': name});
+    }
+
     const changePassword = ({username, currentPassword, newPassword}) => {
         console.log(currentPassword + '.' + newPassword);
-        return api.put(`/users/${username}/password`, {currentPassword, newPassword});
+        return api.put(`/users/${username}/password`,
+            {
+                'currentPassword': currentPassword,
+                'newPassword': newPassword
+            });
     }
+
+    const createPasswordResetToken = (email) => {
+        return api.post(`/users/reset-password`, {'email': email});
+    }
+
+    const resetPassword = ({token, newPassword}) => {
+        return api.put(`/users/reset-password`,
+            {
+                'token': token,
+                'newPassword': newPassword
+            });
+    }
+
+    const sendVerificationToken = (email) => {
+        return api.post(`/users/verification`, {'email': email});
+    }
+
+    const verifyUser = (token) => {
+        return api.put(`/users/verification`, {'token': token})
+    }
+
+    const uploadUserImage = ({username, formData}) => {
+        return api.put(`/users/${username}/image`, formData);
+    }
+
+    const deleteProfileImage = (username) => {
+        return api.delete(`/users/${username}/image`);
+    }
+
+    const createModRequest = (username) => {
+        return api.post(`/users/${username}/mod-requests`);
+    }
+
+    const removeMod = (username) => {
+        return api.delete(`/users/${username}/mod`);
+    }
+
+    const banUser = (username) => {
+        return api.put(`/users/${username}/locked`);
+    }
+
+    const unbanUser = (username) => {
+        return api.delete(`/users/${username}/locked`);
+    }
+
+    const getUserFavoriteMedia = ({username, page, pageSize}) => {
+        return api.get(`/users/${username}/favorite-media`,
+            {
+                params: {
+                    'page': page,
+                    'page-size': pageSize
+                }
+            });
+    }
+
+    const isFavoriteMedia = ({username, mediaId}) => {
+        return api.get(`/users/${username}/favorite-media/${mediaId}`);
+    }
+
+    const addMediaToFavorites = ({username, mediaId}) => {
+        return api.put(`/users/${username}/favorite-media/${mediaId}`);
+    }
+
+    const removeMediaFromFavorites = ({username, mediaId}) => {
+        return api.delete(`/users/${username}/favorite-media/${mediaId}`);
+    }
+
+    const getUserWatchedMedia = ({username, page, pageSize}) => {
+        return api.get(`/users/${username}/watched-media`,
+            {
+                params: {
+                    'page': page,
+                    'page-size': pageSize
+                }
+            });
+    }
+
+    const isWatchedMedia = ({username, mediaId}) => {
+        return api.get(`/users/${username}/watched-media/${mediaId}`);
+    }
+
+    const addMediaToWatched = ({username, mediaId, dateTime}) => {
+        return api.put(`/users/${username}/watched-media/${mediaId}`,
+            {
+                'dateTime': dateTime //"dateTime": "2019-12-03T10:15:30"
+            });
+    }
+
+    const removeMediaFromWatched = ({username, mediaId}) => {
+        return api.delete(`/users/${username}/watched-media/${mediaId}`);
+    }
+
+    const getUserToWatchMedia = ({username, page, pageSize}) => {
+        return api.get(`/users/${username}/to-watch-media`,
+            {
+                params: {
+                    'page': page,
+                    'page-size': pageSize
+                }
+            });
+    }
+
+    const isToWatchMedia = ({username, mediaId}) => {
+        return api.get(`/users/${username}/to-watch-media/${mediaId}`);
+    }
+
+    const addMediaToWatch = ({username, mediaId}) => {
+        return api.put(`/users/${username}/to-watch-media/${mediaId}`);
+    }
+
+    const removeMediaFromToWatch = ({username, mediaId}) => {
+        return api.delete(`/users/${username}/to-watch-media/${mediaId}`);
+    }
+
+    const getUserFavoriteLists = ({username, page, pageSize}) => {
+        return api.get(`/users/${username}/favorite-lists`,
+            {
+                params: {
+                    'page': page,
+                    'page-size': pageSize
+                }
+            });
+    }
+
+    const isFavoriteList = ({username, listId}) => {
+        return api.get(`/users/${username}/favorite-lists/${listId}`);
+    }
+
+    const addListToFavorites = ({username, listId}) => {
+        return api.put(`/users/${username}/favorite-lists/${listId}`);
+    }
+
+    const removeListFromFavorites = ({username, listId}) => {
+        return api.delete(`/users/${username}/favorite-lists/${listId}`);
+    }
+
+    const getUserNotifications = ({username, page, pageSize}) => {
+        return api.get(`/users/${username}/notifications`,
+            {
+                params: {
+                    'page': page,
+                    'page-size': pageSize
+                }
+            });
+    }
+
+    const getUserCollaborationRequests = ({username, page, pageSize}) => {
+        return api.get(`/users/${username}/collab-requests`,
+            {
+                params: {
+                    'page': page,
+                    'page-size': pageSize
+                }
+            });
+    }
+
+    const getUserRecommendedMedia = ({username, page, pageSize}) => {
+        return api.get(`/users/${username}/recommended-media`,
+            {
+                params: {
+                    'page': page,
+                    'page-size': pageSize
+                }
+            });
+    }
+
+    const getUserRecommendedLists = ({username, page, pageSize}) => {
+        return api.get(`/users/${username}/recommended-lists`,
+            {
+                params: {
+                    'page': page,
+                    'page-size': pageSize
+                }
+            });
+    }
+
     return {
         login,
+        getUsers,
         getUser,
-        editUser,
+        createUser,
         deleteUser,
+        editUser,
+        changePassword,
+        createPasswordResetToken,
+        resetPassword,
+        sendVerificationToken,
+        verifyUser,
         uploadUserImage,
-        changePassword
+        deleteProfileImage,
+        createModRequest,
+        removeMod,
+        banUser,
+        unbanUser,
+        getUserFavoriteMedia,
+        isFavoriteMedia,
+        addMediaToFavorites,
+        removeMediaFromFavorites,
+        getUserWatchedMedia,
+        isWatchedMedia,
+        addMediaToWatched,
+        removeMediaFromWatched,
+        getUserToWatchMedia,
+        isToWatchMedia,
+        addMediaToWatch,
+        removeMediaFromToWatch,
+        getUserFavoriteLists,
+        isFavoriteList,
+        addListToFavorites,
+        removeListFromFavorites,
+        getUserNotifications,
+        getUserCollaborationRequests,
+        getUserRecommendedMedia,
+        getUserRecommendedLists
     };
 
 })();
--- frontend/src/services/UserService.js
+++ frontend/src/services/UserService.js
@@ -1,39 +1,230 @@
 import userApi from '../api/UserApi'
+import parse from 'parse-link-header'
 
 const UserService = (function () {
-
-    const getUser = async (username) => {
-        const res = await userApi.getUser(username);
-        return res.data;
-    }
-
-    const editUser = async ({username, name}) => {
-        await userApi.editUser({username, name});
-    }
-
-    const deleteUser = async () => {
-
-    }
 
     const login = async ({username, password}) => {
         const res = await userApi.login({username, password});
         return res.headers.authorization.split(' ')[1];
     }
 
-    const uploadUserImage = async ({username, formData}) => {
-        await userApi.uploadUserImage({username: username, formData: formData});
+    const getUsers = async ({page, pageSize, userRole, banned}) => {
+        const res = await userApi.getUsers({page, pageSize, userRole, banned});
+        const links = parse(res.headers.link);
+        const data = res.data;
+        return {links, data}
+    }
+
+    const getUser = async (username) => {
+        const res = await userApi.getUser(username);
+        return res.data;
+    }
+
+    const createUser = async ({email, username, password, name}) => {
+        await userApi.createUser({email, username, password, name});
+        //el response retorna el header location con la url del usuario nuevo. Se puede llegar a usar
+    }
+
+    const deleteUser = async (username) => {
+        await userApi.deleteUser(username);
+    }
+
+    const editUser = async ({username, name}) => {
+        await userApi.editUser({username, name});
     }
 
     const changeUserPassword = async ({username, currentPassword, newPassword}) => {
         await userApi.changePassword({username, currentPassword, newPassword})
     }
+
+    const createPasswordResetToken = async (email) => {
+        await userApi.createPasswordResetToken(email);
+        //Retorna tanto location del reset-password como una entidad con el token
+    }
+
+    const resetPassword = async ({token, newPassword}) => {
+        await userApi.resetPassword({token, newPassword});
+    }
+
+    const sendVerificationToken = async (email) => {
+        const res = await userApi.sendVerificationToken(email);
+        return res.headers.authorization.split(' ')[1];
+    }
+
+    const verifyUser = async ({token, newPassword}) => {
+        await userApi.verifyUser({token, newPassword});
+    }
+
+    const uploadUserImage = async ({username, formData}) => {
+        await userApi.uploadUserImage({username: username, formData: formData});
+    }
+
+    const deleteProfileImage = async (username) => {
+        await userApi.deleteProfileImage(username);
+    }
+
+    const createModRequest = async (username) => {
+        await userApi.createModRequest(username);
+    }
+
+    const removeMod = async (username) => {
+        await userApi.removeMod(username);
+    }
+
+    const banUser = async (username) => {
+        await userApi.banUser(username);
+    }
+
+    const unbanUser = async (username) => {
+        await userApi.unbanUser(username);
+    }
+
+    const getUserFavoriteMedia = async ({username, page, pageSize}) => {
+        const res = await userApi.getUserFavoriteMedia({username, page, pageSize})
+        const links = parse(res.headers.link);
+        const data = res.data;
+        return {links, data};
+    }
+
+    const isFavoriteMedia = async ({username, mediaId}) => {
+        const res = await userApi.isFavoriteMedia({username, mediaId});
+        return res.data;
+    }
+
+    const addMediaToFavorites = async ({username, mediaId}) => {
+        await userApi.addMediaToFavorites({username, mediaId});
+    }
+
+    const removeMediaFromFavorites = async ({username, mediaId}) => {
+        await userApi.removeMediaFromFavorites({username, mediaId});
+    }
+
+    const getUserWatchedMedia = async ({username, page, pageSize}) => {
+        const res = await userApi.getUserWatchedMedia({username, page, pageSize})
+        const links = parse(res.headers.link);
+        const data = res.data;
+        return {links, data};
+    }
+
+    const isWatchedMedia = async ({username, mediaId}) => {
+        const res = await userApi.isWatchedMedia({username, mediaId});
+        return res.data;
+    }
+
+    const addMediaToWatched = async ({username, mediaId, dateTime}) => {
+        await userApi.addMediaToWatched({username, mediaId, dateTime});
+    }
+
+    const removeMediaFromWatched = async ({username, mediaId}) => {
+        await userApi.removeMediaFromWatched({username, mediaId});
+    }
+
+    const getUserToWatchMedia = async ({username, page, pageSize}) => {
+        const res = await userApi.getUserToWatchMedia({username, page, pageSize})
+        const links = parse(res.headers.link);
+        const data = res.data;
+        return {links, data};
+    }
+
+    const isToWatchMedia = async ({username, mediaId}) => {
+        const res = await userApi.isToWatchMedia({username, mediaId});
+        return res.data;
+    }
+
+    const addMediaToWatch = async ({username, mediaId}) => {
+        await userApi.addMediaToWatch({username, mediaId});
+    }
+
+    const removeMediaFromToWatch = async ({username, mediaId}) => {
+        await userApi.removeMediaFromToWatch({username, mediaId});
+    }
+
+    const getUserFavoriteLists = async ({username, page, pageSize}) => {
+        const res = await userApi.getUserFavoriteLists({username, page, pageSize})
+        const links = parse(res.headers.link);
+        const data = res.data;
+        return {links, data};
+    }
+
+    const isFavoriteList = async ({username, listId}) => {
+        const res = await userApi.isFavoriteList({username, listId});
+        return res.data;
+    }
+
+    const addListToFavorites = async ({username, listId}) => {
+        await userApi.addListToFavorites({username, listId});
+    }
+
+    const removeListFromFavorites = async ({username, listId}) => {
+        await userApi.removeListFromFavorites({username, listId});
+    }
+
+    const getUserNotifications = async ({username, page, pageSize}) => {
+        const res = await userApi.getUserNotifications({username, page, pageSize})
+        const links = parse(res.headers.link);
+        const data = res.data;
+        return {links, data};
+    }
+
+    const getUserCollaborationRequests = async ({username, page, pageSize}) => {
+        const res = await userApi.getUserCollaborationRequests({username, page, pageSize})
+        const links = parse(res.headers.link);
+        const data = res.data;
+        return {links, data};
+    }
+
+    const getUserRecommendedMedia = async ({username, page, pageSize}) => {
+        const res = await userApi.getUserRecommendedMedia({username, page, pageSize})
+        const links = parse(res.headers.link);
+        const data = res.data;
+        return {links, data};
+    }
+
+    const getUserRecommendedLists = async ({username, page, pageSize}) => {
+        const res = await userApi.getUserRecommendedLists({username, page, pageSize});
+        const links = parse(res.headers.link);
+        const data = res.data;
+        return {links, data};
+    }
+
     return {
-        getUser,
-        editUser,
-        deleteUser,
         login,
+        getUsers,
+        getUser,
+        createUser,
+        deleteUser,
+        editUser,
+        changeUserPassword,
+        createPasswordResetToken,
+        resetPassword,
+        sendVerificationToken,
+        verifyUser,
         uploadUserImage,
-        changeUserPassword
+        deleteProfileImage,
+        createModRequest,
+        removeMod,
+        banUser,
+        unbanUser,
+        getUserFavoriteMedia,
+        isFavoriteMedia,
+        addMediaToFavorites,
+        removeMediaFromFavorites,
+        getUserWatchedMedia,
+        isWatchedMedia,
+        addMediaToWatched,
+        removeMediaFromWatched,
+        getUserToWatchMedia,
+        isToWatchMedia,
+        addMediaToWatch,
+        removeMediaFromToWatch,
+        getUserFavoriteLists,
+        isFavoriteList,
+        addListToFavorites,
+        removeListFromFavorites,
+        getUserNotifications,
+        getUserCollaborationRequests,
+        getUserRecommendedMedia,
+        getUserRecommendedLists
     };
 
 })();
