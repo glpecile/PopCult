488f3680419e8e46d8648d80042b67c604824c6c
Paula Domingues <pdomingues@itba.edu.ar>
2022-02-26 17:11:26 -0300

    fix: merge conflict and api timeout

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default axios.create({
     baseURL: process.env.REACT_APP_API_URL,
-    timeout: 1000,
+    timeout: 5000,
     headers: {
         'Authorization': `Bearer ${JSON.parse(localStorage.getItem("userAuthToken")) || JSON.parse(sessionStorage.getItem("userAuthToken")) || ''}`
     }
