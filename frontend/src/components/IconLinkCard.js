8c26c5929f615732de686418b030ce5cd0ab4266
Paula Domingues <pdomingues@itba.edu.ar>
2022-02-27 22:51:16 -0300

    fix: admin panel now has its own card

---
+++ frontend/src/components/IconLinkCard.js
@@ -0,0 +1,20 @@
+import {Link} from "react-router-dom";
+
+const IconLinkCard = (props) => {
+    return (<div
+        className="group flex flex-col bg-white items-center shadow-md hover:shadow-indigo-500/50 rounded-lg p-3 transition duration-300 ease-in-out hover:bg-purple-50 transform hover:-translate-y-1 hover:scale-110">
+        {props.icon}
+        <h2 className="text-2xl text-center py-2 pb-2.5">
+            <Link className="stretched-link text-purple-500 group-hover:text-purple-900"
+                  to={props.linkTo}>
+                <b>
+                    {props.title}
+                </b>
+            </Link>
+        </h2>
+        <p className="p-2.5 m-1.5 text-center">
+            {props.description}
+        </p>
+    </div>);
+}
+export default IconLinkCard;
--- frontend/src/pages/secondary/admin/AdminPanel.js
+++ frontend/src/pages/secondary/admin/AdminPanel.js
@@ -1,6 +1,6 @@
 import {useTranslation} from "react-i18next";
-import {Link} from "react-router-dom";
 import {Helmet} from "react-helmet-async";
+import IconLinkCard from "../../../components/IconLinkCard";
 
 export default function AdminPanel() {
     const isAdmin = true;
@@ -15,54 +15,24 @@ export default function AdminPanel() {
         </h1>
         <div className="grid lg:grid-cols-2 grid-cols-1 gap-5">
             {/*Link to Reports*/}
-            <div
-                className="group flex flex-col bg-white items-center shadow-md hover:shadow-indigo-500/50 rounded-lg p-3 transition duration-300 ease-in-out hover:bg-purple-50 transform hover:-translate-y-1 hover:scale-110">
-                <i className="fas fa-exclamation-circle text-purple-500 group-hover:text-purple-900 fa-2x mt-3"/>
-                <h2 className="text-2xl text-center py-2 pb-2.5">
-                    <Link className="stretched-link text-purple-500 group-hover:text-purple-900"
-                          to='/admin/reports'>
-                        <b>
-                            {t('report_title_plural')}
-                        </b>
-                    </Link>
-                </h2>
-                <p className="p-2.5 m-1.5 text-center">
-                    {t('admin_reports_description')}
-                </p>
-            </div>
+            <IconLinkCard icon={<i
+                className="fas fa-exclamation-circle text-purple-500 group-hover:text-purple-900 fa-2x mt-3"/>}
+                          linkTo='/admin/reports'
+                          title={t('report_title_plural')}
+                          description={t('admin_reports_description')}/>
             {/*Link to Bans*/}
-            <div
-                className="group flex flex-col bg-white items-center shadow-md hover:shadow-indigo-500/50 rounded-lg p-3 transition duration-300 ease-in-out hover:bg-purple-50 transform hover:-translate-y-1 hover:scale-110">
-                <i className="fas fa-user-alt-slash text-purple-500 group-hover:text-purple-900 fa-2x mt-3"/>
-                <h2 className="text-2xl text-center py-2 pb-2.5">
-                    <Link className="stretched-link text-purple-500 group-hover:text-purple-900"
-                          to='/admin/bans'>
-                        <b>
-                            {t('bans_title')}
-                        </b>
-                    </Link>
-                </h2>
-                <p className="p-2.5 m-1.5 text-center">
-                    {t('admin_bans_description')}
-                </p>
-            </div>
+            <IconLinkCard
+                icon={<i className="fas fa-user-alt-slash text-purple-500 group-hover:text-purple-900 fa-2x mt-3"/>}
+                linkTo='/admin/bans'
+                title={t('bans_title')}
+                description={t('admin_bans_description')}/>
             {/*Link to Mods*/}
-            {isAdmin &&
-                <div
-                    className="group flex flex-col bg-white items-center shadow-md hover:shadow-indigo-500/50 rounded-lg p-3 transition duration-300 ease-in-out hover:bg-purple-50 transform hover:-translate-y-1 hover:scale-110">
-                    <i className="fas fa-users text-purple-500 group-hover:text-purple-900 fa-2x mt-3"/>
-                    <h2 className="text-2xl text-center py-2 pb-2.5">
-                        <Link className="stretched-link text-purple-500 group-hover:text-purple-900"
-                              to='/admin/mods'>
-                            <b>
-                                {t('mods_title')}
-                            </b>
-                        </Link>
-                    </h2>
-                    <p className="p-2.5 m-1.5 text-center">
-                        {t('admin_mods_description')}
-                    </p>
-                </div>}
+            {isAdmin && <IconLinkCard
+                icon={<i className="fas fa-users text-purple-500 group-hover:text-purple-900 fa-2x mt-3"/>}
+                linkTo='/admin/mods'
+                title={t('mods_title')}
+                description={t('admin_mods_description')}/>
+            }
         </div>
     </>);
 }
