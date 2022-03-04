92e6f9f7cf5bb35cd579040941331204fa2eb780
Julian Arce <juarce@itba.edu.ar>
2022-03-04 19:33:02 -0300

    chore: Basic Authorizarion Filter class added.

---
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/auth/BasicAuthFilter.java
@@ -0,0 +1,29 @@
+package ar.edu.itba.paw.webapp.auth;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.core.HttpHeaders;
+import java.io.IOException;
+
+@Component
+public class BasicAuthFilter extends OncePerRequestFilter {
+
+    @Autowired
+    JwtTokenUtil jwtTokenUtil;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws ServletException, IOException {
+        final String header = request.getHeader(HttpHeaders.AUTHORIZATION);
+        if(header == null || !header.startsWith("Basic ")) {
+            chain.doFilter(request, response);
+            return;
+        }
+
+    }
+}
