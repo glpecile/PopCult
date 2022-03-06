1529c362b5b796228b189972ef1bc72978522e7a
Roberto J Catalan <rcatalan@itba.edu.ar>
2022-03-06 20:29:23 -0300

    refactor: UserController with MediaDto & null check in auth.

--- webapp/src/main/java/ar/edu/itba/paw/webapp/controller/UserController.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controller/UserController.java
@@ -431,7 +431,7 @@ public class UserController {
             return Response.noContent().build();
         }
 
-        final List<MediaWatchedDto> mediaWatchedDtoList = MediaWatchedDto.fromMediaList(uriInfo, watchedMedia.getElements(), user);
+        final List<MediaWatchedDto> mediaWatchedDtoList = MediaWatchedDto.fromWatchedMediaList(uriInfo, watchedMedia.getElements(), user);
         final Response.ResponseBuilder response = Response.ok(new GenericEntity<List<MediaWatchedDto>>(mediaWatchedDtoList) {
         });
         ResponseUtils.setPaginationLinks(response, watchedMedia, uriInfo);
@@ -666,7 +666,7 @@ public class UserController {
             return Response.noContent().build();
         }
 
-        final List<MediaDto> mediaDtoList = MediaDto.fromMediaList(uriInfo, recommendedMedia.getElements());
+        final List<MediaDto> mediaDtoList = MediaDto.fromMediaList(uriInfo, recommendedMedia.getElements(),user);
         final Response.ResponseBuilder response = Response.ok(new GenericEntity<List<MediaDto>>(mediaDtoList) {
         });
         ResponseUtils.setPaginationLinks(response, recommendedMedia, uriInfo);
