8e00b3bd8990410f3a9c49e4b1d7f4aa24881b14
Gian <gpecile@itba.edu.ar>
2022-04-22 20:36:15 -0300

    feat: media description genre api integration.

--- frontend/src/pages/secondary/media/MediaDescription.js
+++ frontend/src/pages/secondary/media/MediaDescription.js
@@ -2,16 +2,24 @@ import {useCallback, useEffect, useState} from "react";
 import MediaDetails from "../../../components/media/MediaDetails";
 import MediaService from "../../../services/MediaService";
 import Spinner from "../../../components/animation/Spinner";
+import GenreService from "../../../services/GenreService";
+import StudioService from "../../../services/StudioService";
 
-function MediaDescription() {
+function MediaDescription(props) {
     const id = window.location.pathname.split('/')[3];
     const [mediaData, setMediaData] = useState(undefined);
+    const [genreData, setGenreData] = useState(undefined);
 
     const getMedia = useCallback(async () => {
         try {
             const mediaDataToAdd = await MediaService.getMedia(id);
             console.log(mediaDataToAdd);
+            const genreDataToAdd = await GenreService.getMediaGenres(mediaDataToAdd.genreUrl);
+            console.log(genreDataToAdd);
+            // const studiosToAdd = await StudioService.getMediaStudios(mediaDataToAdd.url);
+            // console.log(studiosToAdd);
             setMediaData(mediaDataToAdd);
+            setGenreData(genreDataToAdd);
         } catch (e) {
             console.log(e);
         }
@@ -25,13 +33,14 @@ function MediaDescription() {
 
     return (
         <>
-            {!mediaData &&
-                <Spinner/>
-            }
-            {mediaData && <>
+            {!mediaData && !genreData &&
+                <Spinner/>}
+            {mediaData && genreData && <>
                 <MediaDetails image={mediaData.imageUrl} title={mediaData.title}
                               releaseYear={mediaData.releaseDate.slice(0, 4)} countryName={mediaData.country}
-                              description={mediaData.description}/>
+                              description={mediaData.description}
+                              genres={genreData}
+                />
                 {/* TODO: Listas que contengan a la media */}
                 {/* TODO: Comment Section*/}
             </>}
