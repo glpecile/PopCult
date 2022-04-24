09f038caa7576b88b8bd25c9d0ae40a9c51507e7
Julian Arce <juarce@itba.edu.ar>
2022-04-23 23:38:32 -0300

    chore: NotEmptyBody annotation implemented.
    chore: ValidationMessages properties added. It allows replace java default messages and add custom internationalized messages for annotations.

--- webapp/src/main/java/ar/edu/itba/paw/webapp/dto/validation/annotations/NotEmptyBody.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/dto/validation/annotations/NotEmptyBody.java
@@ -1,6 +1,6 @@
 package ar.edu.itba.paw.webapp.dto.validation.annotations;
 
-import ar.edu.itba.paw.webapp.dto.validation.constraints.ImageValidatorConstraint;
+import ar.edu.itba.paw.webapp.dto.validation.constraints.NotEmptyBodyValidatorConstraint;
 
 import javax.validation.Constraint;
 import javax.validation.Payload;
@@ -9,16 +9,15 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
 import static java.lang.annotation.ElementType.*;
-import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 @Documented
-@Constraint(validatedBy = ImageValidatorConstraint.class)
+@Constraint(validatedBy = NotEmptyBodyValidatorConstraint.class)
 @Target({ TYPE, FIELD, PARAMETER, ANNOTATION_TYPE })
 @Retention(RUNTIME)
-public @interface Image {
+public @interface NotEmptyBody {
 
-    String message() default "{custom.validation.Image.message}";
+    String message() default "{custom.validation.NotEmptyBody.message}";
 
     Class<?>[] groups() default {};
 
