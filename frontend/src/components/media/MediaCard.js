440b4cd8417e33976287eb39287f8ad2b02b6a7c
Gian <gpecile@itba.edu.ar>
2022-04-26 20:16:23 -0300

    fix: "serie" string replaced with "series".
    Backend f******.

--- frontend/src/components/media/MediaCard.js
+++ frontend/src/components/media/MediaCard.js
@@ -1,8 +1,12 @@
 import {Link} from "react-router-dom";
 
 function MediaCard(props) {
+    let mediaType = props.type;
+    if (mediaType.localeCompare("serie") === 0) {
+        mediaType = "series";
+    }
     return (
-        <Link className="text-decoration-none" to={'/media/' + props.type + '/' + props.id}>
+        <Link className="text-decoration-none" to={'/media/' + mediaType + '/' + props.id}>
             <div
                 className="flex flex-col h-full bg-white rounded-lg shadow-md hover:shadow-indigo-500/50 group transition duration-300 ease-in-out hover:bg-violet-50/50 transform hover:-translate-1 hover:scale-105 active:scale-95">
                 <img className="w-full rounded-t-lg" src={props.image} alt="media_image"/>
