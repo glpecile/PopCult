811d75caa440ab6aa5792dec02b4bc708fabe765
Julian Arce <juarce@itba.edu.ar>
2022-05-11 18:06:44 -0300

    fix: i18n implementation.

--- frontend/src/i18n.js
+++ frontend/src/i18n.js
@@ -1,12 +1,20 @@
 import i18n from 'i18next';
-import Backend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
+import localeEN from './locales/en/translation.json';
+import localeES from './locales/es/translation.json';
+
+// https://react.i18next.com/legacy-v9/step-by-step-guide
+const resources = {
+    en: {
+        translation: localeEN
+    },
+    es: {
+        translation: localeES
+    }
+};
 
 i18n
-    // load translation using http -> see /public/locales
-    // learn more: https://github.com/i18next/i18next-http-backend
-    .use(Backend)
     // detect user language
     // learn more: https://github.com/i18next/i18next-browser-languageDetector
     .use(LanguageDetector)
@@ -15,7 +23,10 @@ i18n
     // init i18next
     // for all options read: https://www.i18next.com/overview/configuration-options
     .init({
+        resources,
         fallbackLng: 'en',
+
+        keySeparator: false, // we do not use keys in form messages.welcome
         debug: true,
 
         interpolation: {
