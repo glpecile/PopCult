89212cd64023abf2a420af0c21e3b8ed0a93c5df
Paula Domingues <pdomingues@itba.edu.ar>
2022-05-12 14:08:53 -0300

    fix: exact value removed from router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -32,30 +32,30 @@ export default function App() {
         <Suspense fallback={<Loader/>}>
             <Layout>
                 <Routes>
-                    <Route path='/' exact element={<Home/>}/>
-                    <Route path='/media/films' exact element={<Films/>}/>
-                    <Route path='/media/series' exact element={<Series/>}/>
-                    <Route path='/media/films/:id' exact element={<MediaDescription/>}/>
-                    <Route path='/media/series/:id' exact element={<MediaDescription/>}/>
-                    <Route path='/lists' exact element={<Lists/>}/>
-                    <Route path='/lists/:id' exact element={<ListsDescription/>}/>
-                    <Route path='/lists/new' exact element={<ListsCreation/>}/>
-                    <Route path='/login' exact element={<Login/>}/>
-                    <Route path='/recovery' exact element={<Recovery/>}/>
-                    <Route path='/resetPassword' exact element={<ResetPassword/>}/>
-                    <Route path='/register' exact element={<Register/>}/>
-                    <Route path='/register/success' exact element={<SuccessfulRegister/>}/>
-                    <Route path='/register/expired' exact element={<ExpiredToken/>}/>
-                    <Route path='/user/:username' exact element={<Profile/>}/>
-                    <Route path='/settings' exact element={<Settings/>}/>
-                    <Route path='/verification' exact element={<Verification/>}/>
-                    <Route path='/user/:username/panel' exact element={<UserPanel/>}/>
-                    <Route path='/user/:username/lists' exact element={<UserLists/>}/>
-                    <Route path='/admin' exact element={<AdminPanel/>}/>
-                    <Route path='/admin/reports' exact element={<Reports/>}/>
-                    <Route path='/admin/bans' exact element={<BannedUsers/>}/>
-                    <Route path='/admin/mods' exact element={<Moderators/>}/>
-                    <Route path='/search' exact element={<SearchPage/>}/>
+                    <Route path='/' element={<Home/>}/>
+                    <Route path='/media/films' element={<Films/>}/>
+                    <Route path='/media/series' element={<Series/>}/>
+                    <Route path='/media/films/:id' element={<MediaDescription/>}/>
+                    <Route path='/media/series/:id' element={<MediaDescription/>}/>
+                    <Route path='/lists' element={<Lists/>}/>
+                    <Route path='/lists/:id' element={<ListsDescription/>}/>
+                    <Route path='/lists/new' element={<ListsCreation/>}/>
+                    <Route path='/login' element={<Login/>}/>
+                    <Route path='/recovery' element={<Recovery/>}/>
+                    <Route path='/resetPassword' element={<ResetPassword/>}/>
+                    <Route path='/register' element={<Register/>}/>
+                    <Route path='/register/success' element={<SuccessfulRegister/>}/>
+                    <Route path='/register/expired' element={<ExpiredToken/>}/>
+                    <Route path='/user/:username' element={<Profile/>}/>
+                    <Route path='/settings' element={<Settings/>}/>
+                    <Route path='/verification' element={<Verification/>}/>
+                    <Route path='/user/:username/panel' element={<UserPanel/>}/>
+                    <Route path='/user/:username/lists' element={<UserLists/>}/>
+                    <Route path='/admin' element={<AdminPanel/>}/>
+                    <Route path='/admin/reports' element={<Reports/>}/>
+                    <Route path='/admin/bans' element={<BannedUsers/>}/>
+                    <Route path='/admin/mods' element={<Moderators/>}/>
+                    <Route path='/search' element={<SearchPage/>}/>
                     <Route path='*' element={<Error404/>}/>
                 </Routes>
             </Layout>
