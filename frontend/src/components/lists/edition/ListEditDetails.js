f2be4214b564607d93bd671c2f57447cfa370571
Julian Arce <juarce@itba.edu.ar>
2022-05-20 14:46:37 -0300

    feat: progress circle for text input.

--- frontend/src/components/lists/edition/ListEditDetails.js
+++ frontend/src/components/lists/edition/ListEditDetails.js
@@ -1,10 +1,14 @@
 import {Checkbox, FormControlLabel} from "@mui/material";
 import {useTranslation} from "react-i18next";
+import LengthProgress from "../../comments/LengthProgress";
 
 const ListEditDetails = (props) => {
     const {t} = useTranslation();
     const publicLabel = t('list_public');
     const collaborativeLabel = t('list_collaborative');
+
+    const MAX_LENGTH = 100;
+    const MAX_DESC_LENGTH = 1000;
 
     const handleListName = (event) => {
         if (event.target.validity.valid) {
@@ -31,18 +35,21 @@ const ListEditDetails = (props) => {
                     className="py-2 font-semibold text-xl w-full after:content-['*'] after:ml-0.5 after:text-violet-400">
                     List title
                 </label>
-                <input className="rounded w-full bg-gray-50 text-5xl font-black capitalize justify-start pt-2 break-words max-w-full tracking-wide"
+                <input className="rounded w-full bg-gray-50 text-5xl font-black capitalize justify-start pt-2 break-words max-w-full tracking-wide mb-2"
                        type='text' value={props.listName}
                        pattern='[^<\/>]+'
                        onChange={handleListName}/>
             </div>
+            <LengthProgress length={props.listName.length} max={MAX_LENGTH} text={t('length_count', {current: props.listName.length, max: MAX_LENGTH})}/>
+
             <label
                 className="py-2 font-semibold text-xl w-full mt-2">
                 Description
             </label>
             <textarea
-                className="rounded w-full bg-gray-50 lead text-justify w-full max-w-full break-words mb-2"
+                className="rounded w-full bg-gray-50 lead text-justify w-full max-w-full break-words mb-1"
                 value={props.listDescription} onChange={handleListDescription} placeholder={t('lists_description')}/>
+            <LengthProgress length={props.listDescription.length} max={MAX_DESC_LENGTH} text={t('length_count', {current: props.listDescription.length, max: MAX_DESC_LENGTH})}/>
 
             <div className="flex justify-between pt-1 px-2">
                 <FormControlLabel
